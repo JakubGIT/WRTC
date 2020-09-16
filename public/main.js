383e9d00ec134aa9e960aa098514b1ac333b709f
JakubGIT <jkopecky@uni-osnabrueck.de>
2020-09-17 00:17:10 +0200

    only one TURN

--- public/main.js
+++ public/main.js
@@ -24,8 +24,8 @@ navigator.mediaDevices.getUserMedia(constraints)
         function InitPeer(type) {
             let peer = new Peer({ initiator: (type == 'init') ? true : false, stream: stream, trickle: false,
                 config: { iceServers: [{ urls: 'stun:stun.l.google.com:19302' },
-                { urls: 'stun:stun.gmx.de:3478' }, { url: 'turn:relay.backups.cz', credential: 'webrtc', username: 'webrtc'},
-                { urls: 'turn:relay.backups.cz', credential: 'webrtc', username: 'webrtc' }]}})
+                { urls: 'stun:stun.gmx.de:3478' }, 
+                { url: 'turn:relay.backups.cz', credential: 'webrtc', username: 'webrtc' }]}})
             peer.on('stream', function (stream) {
                 CreateVideo(stream)
             })
